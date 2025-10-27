76506a125aa801c4923d039fed21516e810c96ee
Kimberly Ruiz Ochoa <kimberly290800@hotmail.com>
2025-10-27 14:35:11 -0400

    data load for kaggle

--- data_load.py
+++ data_load.py
@@ -1,1 +1,11 @@
 #File to load Video game data from kaggle
+import kagglehub, pandas as pd, os
+from sqlalchemy import create_engine
+
+def download_and_load():
+    path = kagglehub.dataset_download("beridzeg45/video-games")
+    csv_file = os.path.join(path, "games.csv")
+    df = pd.read_csv(csv_file)
+    engine = create_engine("sqlite:///videogames.db")
+    df.to_sql("games", engine, if_exists="replace", index=False)
+    return engine
